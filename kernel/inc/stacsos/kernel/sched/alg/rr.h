ab78f908e659abb5e1c78afb086351c09de3217d
RobbieLaptop <rm390@st-andrews.ac.uk>
2025-06-12 12:33:02 +0100

    Added Round Robin Scheduler

--- kernel/inc/stacsos/kernel/sched/alg/rr.h
+++ kernel/inc/stacsos/kernel/sched/alg/rr.h
@@ -8,6 +8,7 @@
 #pragma once
 
 #include <stacsos/kernel/sched/alg/scheduling-algorithm.h>
+#include <stacsos/list.h>
 
 namespace stacsos::kernel::sched::alg {
 
@@ -16,9 +17,13 @@ namespace stacsos::kernel::sched::alg {
 
 class round_robin : public scheduling_algorithm {
 public:
-	virtual void add_to_runqueue(tcb &tcb) override;
-	virtual void remove_from_runqueue(tcb &tcb) override;
-	virtual tcb *select_next_task(tcb *current) override;
-	virtual const char *name() const { return "round robin"; }
+  virtual void add_to_runqueue(tcb &tcb) override;
+  virtual void remove_from_runqueue(tcb &tcb) override;
+  virtual tcb *select_next_task(tcb *current) override;
+  virtual const char *name() const { return "round robin"; }
+
+private:
+  // Stores queue to store threads to be run
+  list<tcb *> runqueue_;
 };
 } // namespace stacsos::kernel::sched::alg
